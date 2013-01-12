2fc60e11edbf933d6afc9aab7acaf6e5592b23c8
Pawel Raszewski <pawel.raszewski@teamaol.com>
2013-01-12 14:30:36 +0000

    asd

--- videoDatabaseManager.js
+++ videoDatabaseManager.js
@@ -34,26 +34,7 @@ var fnFindMediaInfo = function(title){
 
 var fnAddVideoToUI = function(url){
 	$.getJSON(url, function(data){
-		if (data["Response"]!="Parse Error"){
-			self.addRowToTable(data,filename);
-			
-			if(previousFilename){
-				filename = previousFilename;
-			}
-			
-			movies.push(filename);
-			if(hasStorage){
-				localStorage.setItem(filename, JSON.stringify(data));
-			}
-			
-		} else {
-			var error = $('<div class="alert-message error hide fade in" data-alert="alert">\
-				<a class="close" href="#">&times;</a><p>Oh snap! Info for <strong>'+filename+'</strong> couldn\'t be found.</p>\
-				<a class="edit" href="#"  onclick="editAndSearchAgain(\'' + filename+ '\',this);">Correct title and search again</a>\
-				</div>');
-            error.appendTo('#message-box');
-            error.fadeIn();
-		}
+		
     });
 }
 
