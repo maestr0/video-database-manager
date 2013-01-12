a21506bc3b4daebc4b40df736d701f5e0fdd0c43
Pawel Raszewski <pawel.raszewski@teamaol.com>
2013-01-12 14:30:00 +0000

    asa

--- videoDatabaseManager.js
+++ videoDatabaseManager.js
@@ -1,3 +1,5 @@
+var self = this;
+
 console.log("Media Database Manager javasrcript ....")
 
 var fnStartApp = function(){
@@ -19,16 +21,40 @@ var fnAddMediaToUI = function(media){
 }
 
 var fnFindMediaInfo = function(title){
-
 	// get list of files
-
+	var url = 'http://www.imdbapi.com/?t='+title;
 	// foreach
 
-	callAPI();
+	fnCallAPI(url);
 	// save
 	fnAddVideoToUI(media)
 
 	
+}
+
+var fnAddVideoToUI = function(url){
+	$.getJSON(url, function(data){
+		if (data["Response"]!="Parse Error"){
+			self.addRowToTable(data,filename);
+			
+			if(previousFilename){
+				filename = previousFilename;
+			}
+			
+			movies.push(filename);
+			if(hasStorage){
+				localStorage.setItem(filename, JSON.stringify(data));
+			}
+			
+		} else {
+			var error = $('<div class="alert-message error hide fade in" data-alert="alert">\
+				<a class="close" href="#">&times;</a><p>Oh snap! Info for <strong>'+filename+'</strong> couldn\'t be found.</p>\
+				<a class="edit" href="#"  onclick="editAndSearchAgain(\'' + filename+ '\',this);">Correct title and search again</a>\
+				</div>');
+            error.appendTo('#message-box');
+            error.fadeIn();
+		}
+    });
 }
 
 fnStartApp();
