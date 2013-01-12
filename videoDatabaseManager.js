2ad2e2b05e8b6c0db7ab881bb535266999ac9331
Pawel Raszewski <pawel.raszewski@teamaol.com>
2013-01-12 14:12:08 +0000

    aaa

--- videoDatabaseManager.js
+++ videoDatabaseManager.js
@@ -1,11 +1,21 @@
-
-
-
 console.log("video Database Manager javasrcript ....")
 
+var fnStartApp = function(){
+	fnBind();
+	fnLoadMoviesFromLocalStorage();
+}
 
+
+var fnLoadMoviesFromLocalStorage = function(){
+
+}
+
+var fnBind = function(){
+
+}
 
 var fnFindMovieInfo = function(title){
 	
 }
 
+fnStartApp();
