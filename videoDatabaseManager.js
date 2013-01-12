0c02e00e18f6db033f69b9598a32bb5c784e1abd
Pawel Raszewski <pawel.raszewski@teamaol.com>
2013-01-12 15:03:56 +0000

    asd

--- videoDatabaseManager.js
+++ videoDatabaseManager.js
@@ -2,42 +2,46 @@ var self = this;
 
 console.log("Media Database Manager javasrcript ....");
 
-var fnStartApp = function(){
-	fnBind();
-	fnLoadMediaFromLocalStorage();
-};
+var fnStartApp = function() {
+		fnBind();
+		fnLoadMediaFromLocalStorage();
+	};
 
 
-var fnLoadMoviesFromLocalStorage = function(){
+var fnLoadMoviesFromLocalStorage = function() {
 
-};
+	};
 
-var fnBind = function(){
+var fnBind = function() {
 
-};
+	};
 
-var fnAddMedia = function(media,media_id){
-	fnAddMediaToUI(media);
-	fnSaveMediaToLocalStorage(media,media_id);
-};
+var fnAddMedia = function(media, media_id) {
+		fnAddMediaToUI(media);
+		fnSaveMediaToLocalStorage(media, media_id);
+	};
 
-var fnAddMediaToUI = function(media){
+var fnAddMediaToUI = function(media) {
 
-};
+	};
 
-var fnFindMediaInfo = function(title){
-	var url = 'http://www.imdbapi.com/?t='+title;
-	fnCallAPI(url);
-};
+var fnFindMediaInfo = function(mediaFile) {
+		// TODO: extract title and year from a file name
+		var title = "Gladiator";
+		var year = "";
 
-var fnCallAPI = function(url){
-	$.getJSON(url, function(data){
-		if (data.Response!="Parse Error"){
-			fnAddMedia(data);						
-		} else {
-			// TODO: error handeling 
-		}
-    });
-};
+		var url = 'http://www.imdbapi.com/?t=' + title + "&y=" + year;
+		fnCallAPI(url);
+	};
+
+var fnCallAPI = function(url) {
+		$.getJSON(url, function(data) {
+			if(data.Response != "Parse Error") {
+				fnAddMedia(data);
+			} else {
+				// TODO: error handeling 
+			}
+		});
+	};
 
 fnStartApp();
