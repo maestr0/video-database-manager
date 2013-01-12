668c1ad688b8791c3f616c7c3ce2e74ec76ed574
Pawel Raszewski <pawel.raszewski@teamaol.com>
2013-01-12 14:59:50 +0000

    asd

--- videoDatabaseManager.js
+++ videoDatabaseManager.js
@@ -1,43 +1,43 @@
 var self = this;
 
-console.log("Media Database Manager javasrcript ....")
+console.log("Media Database Manager javasrcript ....");
 
 var fnStartApp = function(){
 	fnBind();
 	fnLoadMediaFromLocalStorage();
-}
+};
 
 
 var fnLoadMoviesFromLocalStorage = function(){
 
-}
+};
 
 var fnBind = function(){
 
-}
+};
 
 var fnAddMedia = function(media,media_id){
 	fnAddMediaToUI(media);
 	fnSaveMediaToLocalStorage(media,media_id);
-}
+};
 
 var fnAddMediaToUI = function(media){
 
-}
+};
 
 var fnFindMediaInfo = function(title){
 	var url = 'http://www.imdbapi.com/?t='+title;
 	fnCallAPI(url);
-}
+};
 
 var fnCallAPI = function(url){
 	$.getJSON(url, function(data){
-		if (data["Response"]!="Parse Error"){
+		if (data.Response!="Parse Error"){
 			fnAddMedia(data);						
 		} else {
 			// TODO: error handeling 
 		}
     });
-}
+};
 
 fnStartApp();
